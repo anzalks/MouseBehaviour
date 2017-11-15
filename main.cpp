b0aebebd8ebc0147afb04b07d3f6f78a2fff666b
Dilawar Singh <dilawars@ncbs.res.in>
2017-11-15 20:59:02 +0530

    Added nana library.

---
+++ main.cpp
@@ -0,0 +1,23 @@
+/***
+ *       Filename:  main.cpp
+ *
+ *    Description:  Main file.
+ *
+ *        Version:  0.0.1
+ *        Created:  2017-11-15
+ *       Revision:  none
+ *
+ *         Author:  Dilawar Singh <dilawars@ncbs.res.in>
+ *   Organization:  NCBS Bangalore
+ *
+ *        License:  GNU GPL2
+ */
+
+
+
+int main(int argc, const char *argv[])
+{
+    
+    return 0;
+}
+
